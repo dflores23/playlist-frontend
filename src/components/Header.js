69ff1b6ba21d21b144b33c00122478e27dd4e51b
ed <cabrale234@gmail.com>
2021-11-19 21:17:58 -0500

    first changes to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header(props) {
     return <div>
-        <Link to="/"><h1 className="header">Playlist</h1></Link>
+        <Link to="/"><h1 className="header">myPlaylist</h1></Link>
     </div>
 }
 
