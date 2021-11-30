0d427345dd76b01b95ca97f7dfe5582b58329f62
craig-mcgowan <craigmcgowan15@gmail.com>
2021-11-30 01:05:25 -0500

    updates videoinfo

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ import Home from "../pages/Home"
 
 
 const Main = props => {
-  const {createVideo, createPlaylist, deletePlaylist, playlists, videos, getVideos, deleteVideo} = props
+  const {createVideo, createPlaylist, deletePlaylist, playlists, videos, getVideos, deleteVideo, updateVideo} = props
 
 
 
@@ -53,7 +53,7 @@ const Main = props => {
 
           <Route
             path=":id"
-            element={<VidIndex videos={videos} getVideos={getVideos} deleteVideo={deleteVideo} />}
+            element={<VidIndex updateVideo={updateVideo} videos={videos} getVideos={getVideos} deleteVideo={deleteVideo} playlists={playlists}/>}
           />
 
           <Route path="login" element={<Login />} />
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,31 +1,53 @@
 import { useState } from "react";
 const VideoCard = props => {
 
-  const { vidSource, vidTitle, playlists, videoInfo, createVideo } = props
+  const { vidSource, vidTitle, playlists, videoInfo, createVideo, updateVideo, thisVid } = props
   const vidChannel = props.search ? videoInfo.snippet.channelTitle: ""
+  
   const [video, setVideo] = useState({
     playlistName: "",
     vidComments: "",
     playlistID: "",
     vidRating: "",
+    vidName: vidTitle,
     vidUrl: vidSource,
     vidChannel: vidChannel
   });
+  
+  const [update, setUpdate] = useState(
+    !props.search ? (
+      {
+        playlistName: thisVid.playlistName,
+        vidName: vidTitle,
+        vidComments: thisVid.vidComments,
+        playlistID: thisVid.playlistID,
+        vidRating: thisVid.vidRating,
+        vidUrl: vidSource,
+        vidChannel: vidChannel,
+        _id: thisVid._id
+      }) : {}
+    );
+  
   // console.log("This is vidSource: " + vidSource)
   // console.log("This is vidTitle: " + vidTitle)
   // console.log(vidSource, vidTitle)
   const handleChange = (event) => {
-    setVideo({
-      ...video,
-      [event.target.name]: event.target.value,
-    });
+    props.search
+      ? setVideo({
+          ...video,
+          [event.target.name]: event.target.value,
+        })
+      : setUpdate({
+          ...update,
+          [event.target.name]: event.target.value,
+        });
   };
+
   const handleAddToPlaylist = (event) => {
     event.preventDefault();
     let videoCorrected = video;
     videoCorrected.playlistID = playlists
       .filter(playlist=> playlist.playlistName === videoCorrected.playlistName )[0]._id
-    console.log(videoCorrected);
     createVideo(videoCorrected)
     setVideo({
     vidComments: "",
@@ -34,6 +56,16 @@ const VideoCard = props => {
     vidUrl: vidSource,
     vidChannel: vidChannel})
   };
+
+  const handleUpdateVideo = (event) => {
+    event.preventDefault();
+    let updateCorrected = update;
+    updateCorrected.playlistID = playlists
+      .filter(playlist => playlist.playlistName === updateCorrected.playlistName)[0]._id
+    console.log("updateVideo", updateVideo)
+    updateVideo(updateCorrected)
+    alert("Video Information Updated")
+  }
   const getPlaylistOptions = () => {
     return (
       <>
@@ -58,30 +90,68 @@ const VideoCard = props => {
     );
   };
 
-  const displayForm = () => (
-    <form onSubmit={handleAddToPlaylist}>
-          {getPlaylistOptions()}
-          <input
-            placeholder="Comments"
-            value={video.vidComments}
-            onChange={handleChange}
-            name="vidComments"
-          />
+  const displayForm = () => {
+     return props.search ? (
+       <form onSubmit={handleAddToPlaylist}>
+         {getPlaylistOptions()}
+         <input
+           placeholder="Comments"
+           value={video.vidComments}
+           onChange={handleChange}
+           name="vidComments"
+           />
 
-          <input
-            placeholder="Rating"
-            name="vidRating"
-            value={video.vidRating}
-            onChange={handleChange}
-          />
+         <input
+           placeholder="Rating"
+           name="vidRating"
+           value={video.vidRating}
+           onChange={handleChange}
+         />
 
-          <input type="hidden" value={videoInfo.snippet.channelTitle} name="vidChannel" />
-          <input type="hidden" value={vidSource} name="vidUrl" />
-          <input type="hidden" value="" name="playlistID" />
-          <input type="submit" value="+" />
-        </form>
+         <input
+           type="hidden"
+           value={videoInfo.snippet.channelTitle}
+           name="vidChannel"
+         />
+         <input type="hidden" value={vidSource} name="vidUrl" />
+         <input type="hidden" value="" name="playlistID" />
+         <input type="submit" value="+" />
+       </form>
+     ) : (
+       <form onSubmit={handleUpdateVideo}>
+           <input value={update.vidName}
+             onChange={handleChange}
+             placeholder="Video Name"
+             name="vidName"
+             />
+             {getPlaylistOptions()}
+         <input
+           placeholder="Comments"
+           value={update.vidComments}
+           onChange={handleChange}
+           name="vidComments"
+         />
 
-  )
+         <input
+           placeholder="Rating"
+           name="vidRating"
+           value={update.vidRating}
+           onChange={handleChange}
+         />
+
+         <input
+           type="hidden"
+           value={update.vidChannel}
+           name="vidChannel"
+         />
+         <input type="hidden" value={vidSource} name="vidUrl" />
+         <input type="hidden" value="" name="playlistID" />
+         <input type="hidden" value="" name="vidName" />
+         <input type="hidden" value="" name="_id" />
+         <input type="submit" value="update video" />
+       </form>
+     );
+  }
 
   return (
     <>
@@ -96,7 +166,7 @@ const VideoCard = props => {
         allowfullscreen
 
         ></iframe>
-        {props.search ? displayForm(): <></>}
+      {props? displayForm() : <h1>Loading</h1>}
 
     </>
   );
