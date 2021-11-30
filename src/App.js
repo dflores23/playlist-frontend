adc2adeb64dd7c87a368c6dabebed022bd87738b
David Flores <davidafloresvel@gmail.com>
2021-11-30 12:34:02 -0700

    signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Container } from 'react-bootstrap';
 import './App.css';
 import Signup from './components/Signup';
-import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 
 function App() {
-  console.log(process.env)
+ 
   return (
     <AuthProvider>
       <Container className="mt-4 d-flex align-item-center justify-content-center "
@@ -14,7 +15,7 @@ function App() {
         <div className="w-100" style={{ maxWidth: "400px" }}>
 
           <Signup />
-          <Login />
+      
         </div>
       </Container>
     </AuthProvider>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+import {initializeApp} from 'firebase/app';
+import {getAuth} from "firebase/auth"
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -9,8 +9,7 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 
-})
+}
 
-export const auth = app.auth();
-export default app; 
-
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
