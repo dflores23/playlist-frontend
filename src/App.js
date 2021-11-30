cba74ebdc9edc6ebdfa948614489bc82985d41fe
David Flores <davidafloresvel@gmail.com>
2021-11-30 15:55:19 -0700

    signup2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from "react";
+import Signup from './components/Signup';
 import { Container } from 'react-bootstrap';
 import './App.css';
-import Signup from './components/Signup';
 import Login from './components/Login';
+import {Routes, Route} from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext';
 
+import ForgotPassword from "./components/ForgotPassword";
 
 
 function App() {
- 
-  return (
-    <AuthProvider>
-      <Container className="mt-4 d-flex align-item-center justify-content-center "
-        style={{ minHeight: "100vh" }} >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
 
-          <Signup />
-      
-        </div>
-      </Container>
-    </AuthProvider>
+  return (
+    <Container className="mt-4 d-flex align-item-center justify-content-center "
+      style={{ minHeight: "100vh" }} >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <AuthProvider>
+          <Routes>
+          
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </AuthProvider>
+      </div>
+    </Container>
   );
 }
 
